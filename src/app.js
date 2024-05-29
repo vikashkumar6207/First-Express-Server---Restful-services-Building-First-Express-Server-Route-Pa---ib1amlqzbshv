1bad39d525ac98e468b7fdf9e1c8e7825c665f6f
vikashkumar6207 <vktd828403@gmail.com>
2024-05-29 21:24:48 +0530

    complete

--- src/app.js
+++ src/app.js
@@ -7,4 +7,8 @@ app.get('/', (req, res) => {
 });
 
 //Using app.listen create your first express server on port 3000
+app.listen(port,()=>{
+  console.log(`app listining on ${port}`)
+})
+
 module.exports = app;
